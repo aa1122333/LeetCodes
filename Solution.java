4a38726c00f613ebde78acbf703417e040549479
aa1122333 <aa1122333@sina.com>
2016-06-08 22:45:20 +0800

    add 315 233 292 107 241 240

--- Solution.java
+++ Solution.java
@@ -4492,12 +4492,323 @@ public class Solution {
     		change(board, x, y+1);
     	
     }
-    //241. Different Ways to Add Parentheses
-    public List<Integer> diffWaysToCompute(String input) {
+    //315. Count of Smaller Numbers After Self
+    public static List<Integer> countSmaller(int[] nums) {
+    	if(nums==null||nums.length==0) return null;
+    	int [] arr = nums.clone();
+    	Arrays.sort(arr);
+    	int [] bit = new int[nums.length];
+    	List<Integer> sol = new ArrayList<Integer>();
+    	for(int i=0;i<arr.length;i++){
+    		caculate(bit, Arrays.binarySearch(arr, nums[i]), 1);
+    	}
+    	for(int i=0;i<arr.length;i++){
+    		int index = Arrays.binarySearch(arr, nums[i]);
+    		sol.add(sumofall(bit, index-1));
+    		caculate(bit, index, -1);
+    	}
+        return sol;
+    }
+    public static void caculate(int[]bit,int index,int val){
+    	for(int i=index+1;i<bit.length;i+=i&-i)
+    		bit[i] +=val; 
+    }
+    public static int sumofall(int[]bit,int index){
+    	int sum=0;
+    	for(int i=index+1;i>0;i-=i&-i)
+    		sum+=bit[i];
+    	return sum;
+    }
+    //218. The Skyline Problem
+    /*public static List<int[]> getSkyline(int[][] buildings) {
+    	if(buildings==null) return null;
+    	List<int[]> s = new ArrayList<int[]>();
     	
+    	if(buildings.length==0 || buildings[0].length==0) return s;
+    	int max = 0;
+    	
+    	for(int i=0;i<buildings.length;i++){
+    		if(buildings[i][1]>max)
+    			max = buildings[i][1];
+    	}
+    	int []bit = new int[max+1];
+    	for(int i=0;i<buildings.length;i++){
+    		AddVal(bit, buildings[i][0], buildings[i][2], buildings[i][1]);
+    	}
         return null;
+    }
+    public static void AddVal(int[] bit,int index,int curr,int end){
+    	for(int i=index+1;i<end;i+=i&-i){
+    		if(bit[i]<curr)
+    			 bit[i] = curr;
+    			
+    	}
+    }*/
+    public int findIndex(int buildingLeft,List<int[]> re,int StartIndex,int EndIndex){
+        int mid;
+        while(StartIndex<=EndIndex){
+            mid=(StartIndex+EndIndex)/2;
+            if(re.get(mid)[0]==buildingLeft){
+                return mid;
+            }
+            else {
+                if(re.get(mid)[0]<buildingLeft){
+                    StartIndex=mid+1;
+                }
+                else{
+                    EndIndex=mid-1;
+                }
+            }
+        }
+        return StartIndex-1;
+    }
+
+    public int update(List<int[]> re,int index,long buildingLeft,int buildingRight,int buildingHeight){
+
+        int newStart=index;
+
+        for(int i=index;i<re.size();i++){
+            if(i>0&&re.get(i)[1]==re.get(i-1)[1]){
+                re.remove(i);
+                i--;
+                continue;
+            }
+            long thisEnd=(i==re.size()-1)?Long.MAX_VALUE:re.get(i+1)[0];
+            int thisHeight=re.get(i)[1];
+            if(buildingLeft>re.get(i)[0]){
+                if(buildingHeight>re.get(i)[1]){
+                    int[] temp=new int[2];
+                    temp[0]=(int)buildingLeft;
+                    temp[1]=buildingHeight;
+                    re.add(i+1,temp);
+                    newStart=i+1;
+                    i++;
+                }
+            }
+            else{
+                if(buildingHeight>re.get(i)[1]){
+                    re.get(i)[1]=buildingHeight;
+                    if(i>0&&re.get(i-1)[1]==buildingHeight){
+                        re.remove(i);
+                    if(newStart==i)
+                        newStart--;
+                    i--;
+                    }
+                }
+            }
+
+            if(buildingRight<thisEnd){
+                if(buildingHeight>thisHeight){
+                int[] temp=new int[2];
+                temp[0]=buildingRight;
+                temp[1]=thisHeight;
+                re.add(i+1,temp);
+                i++;
+                }
+                break;
+            }
+            else{
+                if(buildingRight==thisEnd){
+                    break;
+                }
+                else{
+                    buildingLeft=thisEnd;
+                }
+            }
+        }
+        return newStart;
+    }
+
+
+    public List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> re=new ArrayList<>();
+        if(buildings.length==0)
+            return re;
+        int[] temp=new int[2];
+        temp[0]=Integer.MIN_VALUE;
+        temp[1]=0;
+        re.add(temp);
+        int StartIndex=0;
+        for(int i=0;i<buildings.length;i++){
+            int buildingLeft=buildings[i][0];
+            int buildingRight=buildings[i][1];
+            int buildingHeight=buildings[i][2];
+            int index=findIndex(buildingLeft,re,StartIndex,re.size()-1);
+            StartIndex=update(re,index,buildingLeft,buildingRight,buildingHeight);
+        }
+        re.remove(0);
+        return re;
+    }
+    //233. Number of Digit One
+    public static int countDigitOne(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        int count = 0;
+        int copyN = n;
+        for (int weight = 1; n > 0; weight *= 10) {
+            int complete = n / 10;
+            int lsb = n % 10;
+            if (lsb < 1) {
+                count += weight * complete;
+            } else if (lsb > 1) {
+                count += weight * (complete + 1);
+            } else {  
+                count += weight * complete + copyN % weight + 1;
+            }
+            n = complete;
+        }
+        return count;
+    }
+    //292. Nim Game
+    public boolean canWinNim(int n) {
+        if(n%4==0)return false;
+        else return true;
+    }
+    //107. Binary Tree Level Order Traversal II
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Stack<List<Integer>> s = new Stack<List<Integer>>();
+        if(root == null) return s;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        while(!q.isEmpty()){
+        	List<Integer> t = new ArrayList<Integer>();
+        	List<TreeNode> tn = new ArrayList<TreeNode>();
+        	while(!q.isEmpty())
+        		tn.add(q.poll());
+        	for(int i=0;i<tn.size();i++){
+        		TreeNode tmp = tn.get(i);
+        		t.add(tmp.val);
+        		if(tmp.left!=null) q.add(tmp.left);
+        		if(tmp.right!=null) q.add(tmp.right);
+        	}
+        	s.push(t);
+        }
+        /*List<List<Integer>> sol = new ArrayList<List<Integer>>();
+        for(int i=s.size()-1;i>=0;i--){
+        	sol.add(s.get(i));
+        }*/
+        
+        return s;
+    }
+     //241. Different Ways to Add Parentheses
+    static Map<String, List<Integer>> map = new HashMap<>();
+    public static List<Integer> diffWaysToCompute(String input) {
+    	if(map.containsKey(input))
+            return map.get(input);
+    	List<Integer> s = new ArrayList<Integer>();
+    	for(int i=0;i<input.length();i++){
+    		char t = input.charAt(i);
+    		if(t=='+'||t=='-'||t=='*'){
+    			String a = input.substring(0, i);
+    			String b = input.substring(i+1);
+    			List<Integer> t1 = diffWaysToCompute(a);
+    			List<Integer> t2 = diffWaysToCompute(b);
+    			for(int ta:t1)
+    				for(int tb:t2){
+    					if(t=='+')
+    						s.add(ta+tb);
+    					else if(t=='-')
+    						s.add(ta-tb);
+    					else if(t=='*')
+    						s.add(ta*tb);
+    					
+    				}
+    		}
+    	}
+    	
+    	if(s.size()==0)  s.add(Integer.valueOf(input));
+    	map.put(input, s);
+        return s;
 
     }
+    
+    public static class Node{
+    	public int x;
+    	public int y;
+    	public Node(int x,int y){
+    		this.x = x;
+    		this.y = y;
+    	}
+    }
+    
+    //240. Search a 2D Matrix II
+    public static  boolean searchMatrix_240(int[][] matrix, int target) {
+    	int x = matrix.length;
+    	if(x==0) return false;
+    	int y = matrix[0].length;
+    	if(y==0) return false;
+    	
+    	return search_240(0, x-1, 0, y-1, matrix, target);
+    }
+    
+    public static boolean search_240(int x1,int x2,int y1,int y2,int[][]matrix,int target){
+    	if(x1>x2||y1>y2) return false;
+    	if(target<matrix[x1][y1]||target>matrix[x2][y2]) return false;
+    	
+    	int x = x1+((x2-x1)>>>1);
+    	int y = y1+((y2-y1)>>>1);
+    	int t = matrix[x][y];
+    	if(t==target) return true;
+    	else if(t<target){
+
+    		return  search_240(x1,x,y+1,y2,matrix,target)||search_240(x+1, x2, y1, y2, matrix, target);
+    	}
+    	else  return search_240(x1,x-1,y1,y2,matrix,target)||search_240(x1, x2, y1, y-1, matrix, target);
+    	
+    }
+    
+    //240-2
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int line=0;
+        int len=matrix.length;
+        int col=matrix[0].length;
+        while (line<len && col>0){
+            int num= matrix[line][col-1];
+            if (num>target) col--;
+            if (num<target) line++;
+            if (num==target) return true;}
+        
+        return false;
+    }
+    
+    public static int binarysearch_(int []a,int start,int end,int key){
+    	int from = start;
+    	int to = end;
+    	while(from<to){
+    		int mid = (from+to)>>>1;
+            @SuppressWarnings("rawtypes")
+			Comparable c = (Comparable)a[mid];
+            @SuppressWarnings("unchecked")
+			int p = c.compareTo(key);
+            if(p>key)
+            	to = mid-1;
+            else if(p<key)
+            	from = mid +1;
+            else 
+            	return mid;
+    	}
+    	return -1;
+    }
+    
+    public static int countTheTriangle(int n){
+    	if(n==0) return 0;
+    	int sum=1;
+    	if(n>=2){
+    		sum+=n*n;
+    		for(int i=n-1;i>1;i--){
+    			sum+=(1+i)*i/2;
+    		}
+    		if(n>=4){
+    			for(int i=n-3;i>0;i-=2){
+    				sum+=(1+i)*i/2;
+    			}
+    		}
+    	}
+    	return sum;
+    }
+    
+    
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
@@ -4843,10 +5154,26 @@ public class Solution {
 		System.out.println( p = intersect(s,s2));*/
 		
 
-		int[] h = {5334,6299,4199,9663,8945,3566,9509,3124,6026,6250,7475,5420,9201,9501,38,5897,4411,6638,9845,161,9563,8854,3731,5564,5331,4294,3275,1972,1521,2377,3701,6462,6778,187,9778,758,550,7510,6225,8691,3666,4622,9722,8011,7247,575,5431,4777,4032,8682,5888,8047,3562,9462,6501,7855,505,4675,6973,493,1374,3227,1244,7364,2298,3244,8627,5102,6375,8653,1820,3857,7195,7830,4461,7821,5037,2918,4279,2791,1500,9858,6915,5156,970,1471,5296,1688,578,7266,4182,1430,4985,5730,7941,3880,607,8776,1348,2974,1094,6733,5177,4975,5421,8190,8255,9112,8651,2797,335,8677,3754,893,1818,8479,5875,1695,8295,7993,7037,8546,7906,4102,7279,1407,2462,4425,2148,2925,3903,5447,5893,3534,3663,8307,8679,8474,1202,3474,2961,1149,7451,4279,7875,5692,6186,8109,7763,7798,2250,2969,7974,9781,7741,4914,5446,1861,8914,2544,5683,8952,6745,4870,1848,7887,6448,7873,128,3281,794,1965,7036,8094,1211,9450,6981,4244,2418,8610,8681,2402,2904,7712,3252,5029,3004,5526,6965,8866,2764,600,631,9075,2631,3411,2737,2328,652,494,6556,9391,4517,8934,8892,4561,9331,1386,4636,9627,5435,9272,110,413,9706,5470,5008,1706,7045,9648,7505,6968,7509,3120,7869,6776,6434,7994,5441,288,492,1617,3274,7019,5575,6664,6056,7069,1996,9581,3103,9266,2554,7471,4251,4320,4749,649,2617,3018,4332,415,2243,1924,69,5902,3602,2925,6542,345,4657,9034,8977,6799,8397,1187,3678,4921,6518,851,6941,6920,259,4503,2637,7438,3893,5042,8552,6661,5043,9555,9095,4123,142,1446,8047,6234,1199,8848,5656,1910,3430,2843,8043,9156,7838,2332,9634,2410,2958,3431,4270,1420,4227,7712,6648,1607,1575,3741,1493,7770,3018,5398,6215,8601,6244,7551,2587,2254,3607,1147,5184,9173,8680,8610,1597,1763,7914,3441,7006,1318,7044,7267,8206,9684,4814,9748,4497,2239};
-		System.out.println(minSubArrayLen(697439,h));
-        
+/*		int[] s = {5,2,6,1};
+		System.out.println(countSmaller(s));*/
+/*		int [] nums = {1,3,5};
+		NumArray s = new NumArray(nums);
+		System.out.println(s.sumRange(0, 2));*/
+		/*TreeNode t = new TreeNode(5);
+		t.left = new TreeNode(4);
+		t.right = new TreeNode(8);
+		t.left.left = new TreeNode(11);
+		t.right.left = new TreeNode(13);
+		t.right.right = new TreeNode(4);
+		t.right.right.left = new TreeNode(5);
+		t.right.right.right = new TreeNode(1);
+		t.left.left.right = new TreeNode(2);
+		t.left.left.left = new TreeNode(7);
+		System.out.println(levelOrderBottom(t));*/
 
+		System.out.println(countTheTriangle(3));
+
+		
 		System.out.println();
 		System.out.println((System.nanoTime()-time)/1000000+"ms");
 	}
