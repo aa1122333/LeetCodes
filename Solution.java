c8028b0440f41d07a7f387e5566b9a9cb6d0d3a7
aa1122333 <aa1122333@sina.com>
2016-06-30 23:55:55 +0800

    add 278

--- Solution.java
+++ Solution.java
@@ -6744,6 +6744,21 @@ public class Solution {
         }
         return sol.next;
     }
+    
+    //278. First Bad Version
+    /*public static int firstBadVersion(int n) {
+        int start = 1, end = n;
+        while (start < end) {
+            int mid = start + (end-start) / 2;
+            if (!isBadVersion(mid)) start = mid + 1;
+            else end = mid;            
+        }        
+        return start;
+    }*/
+    //28. Implement strStr()
+    public static int strStr(String haystack, String needle) {
+        
+    }
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
