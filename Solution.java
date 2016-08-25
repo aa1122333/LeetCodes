89e244ded41ded74be7383ca1a21aff0a345ed70
aa1122333 <aa1122333@sina.com>
2016-08-25 21:33:43 +0800

    add 289 321

--- Solution.java
+++ Solution.java
@@ -8448,6 +8448,160 @@ public class Solution {
             return (int)Math.pow(3, s-1)*4;
         return 1;
     }
+    //289. Game of Life
+    public static void gameOfLife(int[][] board) {
+    	int x = board.length;
+    	int y = 0;
+    	if(x!=0)
+    	y = board[0].length;
+    	if(x==0 || y==0) return ;
+        int [][] sol = new int[x][y];
+        for(int i=0;i<x;i++){
+        	for(int j=0;j<y;j++){
+        		if(board[i][j]==1){
+        			if(i-1>=0){
+        				if(j-1>=0)
+        					sol[i-1][j-1] +=1;
+        				if(j>=0)
+        					sol[i-1][j] +=1;
+        				if(j+1<y)
+        					sol[i-1][j+1] +=1;
+        			}
+        			if(i>=0){
+        				if(j-1>=0)
+        					sol[i][j-1] +=1;
+        				
+        				if(j+1<y)
+        					sol[i][j+1] +=1;
+        			}
+        			if(i+1<x){
+        				if(j-1>=0)
+        					sol[i+1][j-1] +=1;
+        				if(j>=0)
+        					sol[i+1][j] +=1;
+        				if(j+1<y)
+        					sol[i+1][j+1] +=1;
+        			}
+        		}
+        	}
+        }
+        /*for(int i=0;i<x;i++){
+        	for(int j=0;j<y;j++){
+        		System.out.print(sol[i][j]+" ");
+        	}
+        	System.out.println();
+        }*/
+        
+        for(int i=0;i<x;i++){
+        	for(int j=0;j<y;j++){
+        		if(board[i][j]==1){
+        			if(sol[i][j]==2||sol[i][j]==3)
+        				board[i][j] = 1;
+        			else if(sol[i][j]<2 || sol[i][j]>3)
+        				board[i][j] = 0;
+        			
+        		}
+        		if(board[i][j]==0 && sol[i][j]==3)
+        			board[i][j] = 1;
+        	}
+        }
+    }
+    //321. Create Maximum Number
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+
+        int[] maxNumber = new int[k];
+
+        int[] leftClosest = getClosest(nums1);
+        int[] rightClosest = getClosest(nums2);
+
+        List<State> states = new LinkedList<State>();
+        List<State> nextStates = new LinkedList<State>();
+        states.add(new State(0, 0));
+
+        boolean[][] stateSet = new boolean[nums1.length + 1][nums2.length + 1];
+        stateSet[0][0] = true;
+        for (int i = 0; i < k; i++) {
+            int max = -1;// max digit
+            for (State state : states) {
+                stateSet[state.x][state.y] = false;
+                int remainingDigits = k - i;
+                int nextLeft = getNextIndex(leftClosest, state.x, remainingDigits - (nums2.length - state.y));
+                int nextRight = getNextIndex(rightClosest, state.y, remainingDigits - (nums1.length - state.x));
+                if (nextLeft != -1) {
+                    max = Math.max(max, nums1[nextLeft]);
+                }
+                if (nextRight != -1) {
+                    max = Math.max(max, nums2[nextRight]);
+                }
+            }
+            maxNumber[i] = max;
+
+            while (!states.isEmpty()) {
+                State state = states.remove(states.size() - 1);
+                int remainingDigits = k - i;
+                int nextLeft = getNextIndex(leftClosest, state.x, remainingDigits - (nums2.length - state.y));
+                int nextRight = getNextIndex(rightClosest, state.y, remainingDigits - (nums1.length - state.x));
+                if (nextLeft != -1) {
+                    if (nums1[nextLeft] == max && stateSet[nextLeft + 1][state.y] == false) {
+                        stateSet[nextLeft + 1][state.y] = true;
+                        nextStates.add(new State(nextLeft + 1, state.y));
+                    }
+                }
+                if (nextRight != -1) {
+                    if (nums2[nextRight] == max && stateSet[state.x][nextRight + 1] == false) {
+                        stateSet[state.x][nextRight + 1] = true;
+                        nextStates.add(new State(state.x, nextRight + 1));
+                    }
+                }
+            }
+            List<State> temp = states;
+            states = nextStates;
+            nextStates = temp;
+        }
+
+        return maxNumber;
+    }
+
+    private class State {
+        int x, y;
+
+        public State(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private int getNextIndex(int[] closest, int start, int needed) {
+        int result = -1;
+        if (start == closest.length) {
+            return -1;
+        }
+        int index = start;
+        while (index != -1 && closest.length - index >= needed) {
+            result = index;
+            index = closest[index];
+        }
+
+        return result;
+    }
+
+    private int[] getClosest(int[] A) {
+        int[] closest = new int[A.length];
+           if(A.length==0) {
+            return closest;
+        }
+        closest[A.length - 1] = -1;
+
+        for (int i = A.length - 2; i >= 0; i--) {
+            int j = i + 1;
+            while (j != -1 && A[i] >= A[j]) {
+                j = closest[j];
+            }
+            closest[i] = j;
+        }
+        return closest;
+    }
+
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
@@ -9135,7 +9289,15 @@ public class Solution {
 		root.next.next.next.next = new ListNode(2);
 		root.next.next.next.next.next = new ListNode(1);*/
 		/*System.out.println(isPalindrome_234(root));*/
-		System.out.println(integerBreak(2));
+		/*System.out.println(integerBreak(2));*/
+		int [][]s = {
+				{0,1,0,0,1},
+				{0,1,1,0,1},
+				{0,0,0,1,1},
+				{0,0,0,0,1},
+				{0,0,0,0,1}
+		};
+		gameOfLife(s);
 		System.out.println();
 		System.out.println((System.nanoTime()-time)/1000000+"ms");
 	}
